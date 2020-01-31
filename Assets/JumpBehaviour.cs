13f03d9a98f9d8ea8a1d7dc560632f6bc9e27aef
Samuel <samuelsoutullosobral@gmail.com>
2020-01-31 19:34:29 +0100

    Good jumping curve

---
+++ Assets/JumpBehaviour.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JumpBehaviour : MonoBehaviour
+{
+    // Editor
+    public float jumpVelocity = 5;
+    public float fallMultiplier = 2.5f;
+    public float lowJumpMultiplier = 2f;
+
+    // Refs
+    private Rigidbody2D rb;
+
+    // State
+    private bool jumpPressed = false;
+    private bool holdingJump = false;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+
+    private void Update()
+    {
+        if(Input.GetButtonDown("Jump"))
+        {
+            jumpPressed = true;
+            holdingJump = true;
+        }
+
+        if(Input.GetButtonUp("Jump"))
+        {
+            holdingJump = false;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if(jumpPressed)
+        {
+            rb.velocity = Vector2.up * jumpVelocity;
+        }
+
+        if (rb.velocity.y < 0)
+        {
+            rb.velocity += Vector2.up * Physics2D.gravity.y * (fallMultiplier - 1) * Time.fixedDeltaTime;
+        }
+        else if(rb.velocity.y > 0 && !holdingJump)
+        {
+            rb.velocity += Vector2.up * Physics2D.gravity.y * (fallMultiplier - 1) * Time.fixedDeltaTime;
+        }
+
+        jumpPressed = false;
+    }
+}
