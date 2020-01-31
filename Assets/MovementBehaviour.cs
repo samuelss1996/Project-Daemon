d6980ea772d45b79b5c04a2f157c062667ca8492
Samuel <samuelsoutullosobral@gmail.com>
2020-01-31 20:30:23 +0100

    Basic movement working

---
+++ Assets/MovementBehaviour.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementBehaviour : MonoBehaviour
+{
+    // Editor
+    public float maxSpeed;
+
+    // State
+    private float axis;
+
+
+    void Update()
+    {
+        axis = Input.GetAxis("Horizontal");
+        Debug.Log(axis);
+    }
+
+    private void FixedUpdate()
+    {
+        Vector2 currentVelocity = GetComponent<Rigidbody2D>().velocity;
+        currentVelocity.x = axis * maxSpeed;
+
+        GetComponent<Rigidbody2D>().velocity = currentVelocity;
+    }
+}
