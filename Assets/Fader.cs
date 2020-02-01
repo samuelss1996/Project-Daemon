d78ea4e119179deed16a8f7e0b21287dff37c550
Samuel <samuelsoutullosobral@gmail.com>
2020-02-01 18:25:47 +0100

    Added fade

---
+++ Assets/Fader.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Fader : MonoBehaviour
+{
+    // Editor
+    public float fadeTime;
+
+    // Refs
+    private Image image;
+
+    // State
+    private Color start = Color.clear;
+    private Color end = Color.clear;
+    private float currentTime;
+
+    private void Awake()
+    {
+        image = GetComponent<Image>();
+    }
+
+    void Update()
+    {
+        image.color = Color.Lerp(start, end, Mathf.SmoothStep(0, 1, currentTime / fadeTime));
+        currentTime += Time.deltaTime;
+    }
+
+    public void FadeIn()
+    {
+        Fade(Color.black, Color.clear);
+    }
+
+    public void FadeOut()
+    {
+        Fade(Color.clear, Color.black);
+    }
+
+    private void Fade(Color _start, Color _end)
+    {
+        start = _start;
+        end = _end;
+
+        currentTime = 0;
+    }
+}
---
+++ Assets/LevelTransitioner.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelTransitioner : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        FindObjectOfType<Fader>().FadeIn();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown("t"))
+        {
+            NextLevel();
+        }
+    }
+
+    private void NextLevel()
+    {
+        FindObjectOfType<Fader>().FadeOut();
+    }
+}
