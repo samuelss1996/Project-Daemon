952abe6c2fe14dce63f446d5e26d8c96a8ba85b3
Samuel <samuelsoutullosobral@gmail.com>
2020-02-01 14:27:04 +0100

    Moving platform is now moving

---
+++ Assets/MovingPlatform.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingPlatform : MonoBehaviour
+{
+    // Editor
+    public Vector3 relativeEndPosition;
+    public float speed;
+
+    // State
+    private Vector3 startPosition;
+
+    private void Awake()
+    {
+        startPosition = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float normalizedSpeed = Mathf.Sqrt(speed / relativeEndPosition.magnitude);
+        float currentLerp = -Mathf.Cos(Time.time * normalizedSpeed) * 0.5f + 0.5f;
+
+        transform.position = Vector3.Lerp(startPosition, startPosition + relativeEndPosition, currentLerp);
+    }
+}
