cdae9ec1360790c6d697dff7a95e2fc6bd091214
Samuel <samuelsoutullosobral@gmail.com>
2020-02-01 15:06:28 +0100

    Fragile platform working

--- Assets/FragilePlatform.cs
+++ Assets/FragilePlatform.cs
@@ -9,14 +9,25 @@ public class FragilePlatform : MonoBehaviour
     public float shakeSpeed;
     public float shakeStrength;
 
+    public float fadeTime;
+    public float restartTime;
+
+    // Refs
+    private SpriteRenderer sprite;
+
     // State
     private bool shouldFall;
+    private bool allowCycleStart;
     private float timeSinceTouch;
     private Vector2 originalPos;
+    private Color originalColor;
 
     private void Awake()
     {
+        sprite = GetComponent<SpriteRenderer>();
         originalPos = transform.position;
+        originalColor = sprite.color;
+        allowCycleStart = true;
     }
 
     // Update is called once per frame
@@ -29,7 +40,26 @@ public class FragilePlatform : MonoBehaviour
                 float currentShakePosX = Mathf.Sin(timeSinceTouch * shakeSpeed) * shakeStrength;
                 float currentShakePosY = (Mathf.Cos(timeSinceTouch * shakeSpeed * 1.3f) - 1) * shakeStrength / 2;
 
+                sprite.color = originalColor;
                 transform.position = originalPos + new Vector2(currentShakePosX, currentShakePosY);
+            }
+            else if(timeSinceTouch <= shakeTime + fadeTime)
+            {
+                SetSmoothAlpha((timeSinceTouch - shakeTime) / fadeTime, true);
+            }
+            else if(timeSinceTouch <= shakeTime + fadeTime + restartTime)
+            {
+                gameObject.GetComponent<BoxCollider2D>().enabled = false;
+            }
+            else if(timeSinceTouch <= shakeTime + 2 * fadeTime + restartTime)
+            {
+                gameObject.GetComponent<BoxCollider2D>().enabled = true;
+                SetSmoothAlpha((timeSinceTouch - shakeTime - fadeTime - restartTime) / fadeTime, false);
+                allowCycleStart = true;
+            }
+            else
+            {
+                shouldFall = false;
             }
 
             timeSinceTouch += Time.deltaTime;
@@ -38,10 +68,23 @@ public class FragilePlatform : MonoBehaviour
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if(!shouldFall)
+        if(allowCycleStart)
         {
+            allowCycleStart = false;
             shouldFall = true;
             timeSinceTouch = 0;
         }
+    }
+
+    private void SetSmoothAlpha(float lerp, bool startOpaque)
+    {
+        float min = startOpaque ? 1 : 0;
+        float max = startOpaque ? 0 : 1;
+
+        float currentAlpha = Mathf.SmoothStep(min, max, lerp);
+        Color currentColor = originalColor;
+        currentColor.a = currentAlpha;
+
+        sprite.color = currentColor;
     }
 }
