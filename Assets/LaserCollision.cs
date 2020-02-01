ba8d4a8cb4c957e69f86d60ada6ad3c52dcf72d0
Samuel <samuelsoutullosobral@gmail.com>
2020-02-01 16:10:18 +0100

    Purple laser following player and colliding

---
+++ Assets/LaserCollision.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LaserCollision : MonoBehaviour
+{
+    // Editor
+    public float maxDistance;
+
+    // Refs
+    private LineRenderer line;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        line = GetComponent<LineRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, transform.up);
+        line.SetPosition(0, transform.position);
+
+        if (hit.collider != null) {
+            line.SetPosition(1, transform.position + hit.distance * transform.up);
+        }
+        else
+        {
+            line.SetPosition(1, transform.position + maxDistance * transform.up);
+        }
+    }
+}
---
+++ Assets/LaserFollowPlayer.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LaserFollowPlayer : MonoBehaviour
+{
+    // Editor
+    public float speed;
+
+    // Refs
+    private GameObject player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 diff = player.transform.position - transform.position;
+        diff.Normalize();
+
+        float rot_z = Mathf.Atan2(diff.y, diff.x) * Mathf.Rad2Deg;
+        Quaternion targetRot = Quaternion.Euler(0f, 0f, rot_z - 90);
+
+        transform.rotation = Quaternion.RotateTowards(transform.rotation, targetRot, speed * Time.deltaTime * Quaternion.Angle(transform.rotation, targetRot));
+    }
+}
