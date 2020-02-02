c15cfcf69c48d6df44f49c9d006d577826739b8d
Samuel <samuelsoutullosobral@gmail.com>
2020-02-02 03:46:22 +0100

    Level 5 layout done

--- Assets/Scripts/AutofallPlatform.cs
+++ Assets/Scripts/AutofallPlatform.cs
@@ -4,8 +4,6 @@ using UnityEngine;
 
 public class AutofallPlatform : MonoBehaviour
 {
-    private static int platformsCount = 0;
-
     // Editor
     public static float startTime = 3f;
     public static float spacingTime = 0.2f;
@@ -16,25 +14,23 @@ public class AutofallPlatform : MonoBehaviour
     private SpriteRenderer sprite;
 
     // State
-    private int index;
     private Vector2 originalPos;
     private Color originalColor;
+    private bool fall;
+    private float currentTime;
 
     // Start is called before the first frame update
     void Awake()
     {
-        index = platformsCount;
         sprite = GetComponent<SpriteRenderer>();
         originalPos = transform.position;
         originalColor = sprite.color;
-
-        platformsCount++;
     }
 
     // Update is called once per frame
     void Update()
     {
-        float currentAnimationTime = Time.time - (startTime + index * spacingTime);
+        /*float currentAnimationTime = Time.time - (startTime + index * spacingTime);
         if (currentAnimationTime >= 0)
         {
             float animTimeNormalized = currentAnimationTime / animationTime;
@@ -47,6 +43,29 @@ public class AutofallPlatform : MonoBehaviour
 
             SpriteUtils.SetSmoothAlpha(sprite, originalColor, animTimeNormalized, true);
             //transform.position = Vector3.Slerp(originalPos, originalPos + fallDownDistance * Vector2.down, animTimeNormalized);
+        }*/
+
+        if(fall)
+        {
+            float animTimeNormalized = currentTime / animationTime;
+
+            if (animTimeNormalized >= 1)
+            {
+                gameObject.SetActive(false);
+                return;
+            }
+
+            SpriteUtils.SetSmoothAlpha(sprite, originalColor, animTimeNormalized, true);
+
+            currentTime += Time.deltaTime;
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.CompareTag("AutofallTrigger"))
+        {
+            fall = true;
         }
     }
 }
---
+++ Assets/Scripts/AutofallTriggerBehaviour.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AutofallTriggerBehaviour : MonoBehaviour
+{
+    // Editor
+    public float velocity;
+
+    public void StartMoving()
+    {
+        GetComponent<Rigidbody2D>().velocity = new Vector2(velocity, 0);
+    }
+}
