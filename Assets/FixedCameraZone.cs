df98a823e2d6eaf6b9748551478e8684822418ae
Samuel <samuelsoutullosobral@gmail.com>
2020-02-02 04:02:05 +0100

    Camera transition in boss room

---
+++ Assets/FixedCameraZone.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FixedCameraZone : MonoBehaviour
+{
+    // Editor
+    public float moveTime;
+
+    // State
+    private Vector3 initialPos;
+    private float currentTime;
+    private bool move;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(move)
+        {
+            Camera.main.gameObject.transform.position = Vector3.Slerp(initialPos, transform.position, currentTime / moveTime);
+
+            if(currentTime / moveTime >= 1)
+            { 
+                gameObject.SetActive(false);
+            }
+
+            currentTime += Time.deltaTime;
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.CompareTag("Player"))
+        {
+            Camera.main.gameObject.GetComponent<DeadzoneCamera>().enabled = false;
+            initialPos = Camera.main.gameObject.transform.position;
+            move = true;
+        }
+    }
+}
